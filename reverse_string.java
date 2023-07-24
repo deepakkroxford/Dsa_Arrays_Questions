b787342ab30d23bf719cea1de6cdeb4e6954ae7a
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-07-24 22:13:22 +0530

    Update reverse_string.java

--- reverse_string.java
+++ reverse_string.java
@@ -1,3 +1,9 @@
+// You are given a string s. You need to reverse the string.
+// You only need to complete the function reverseWord() that takes s as parameter and returns the reversed string.
+// Expected Time Complexity: O(|S|).
+// Expected Auxiliary Space: O(1).
+// s = Geeks
+// Output: skeeG
 public class reverse_string {
     public static void main(String[] args) {
          // Reverse the string str
