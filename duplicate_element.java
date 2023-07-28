d5806a69aec9db2797b34dced22bdfb9794394a2
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-07-29 02:01:29 +0530

    d

---
+++ duplicate_element.java
@@ -0,0 +1,21 @@
+public class duplicate_element {
+    public static void main(String[] args) {
+        int arr[]= {1,2,3,3,4,5};
+
+        for(int i =0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    System.out.println("duplicate element is"+arr[i]);
+                }
+               
+            }
+            
+        }
+
+
+    }
+    
+}
