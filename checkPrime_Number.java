e963079cd006e2bf4f159933049504e3d5d07114
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-07-31 23:17:47 +0530

    basics

---
+++ checkPrime_Number.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;;
+public class checkPrime_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of n");
+        int N =sc.nextInt();
+        int count =0;
+
+        if(N<=1)
+        {
+            System.out.println("not prime");
+
+        }
+       
+        
+            for(int i=1;i<N;i++)
+            {
+                
+                if(N%i==0)
+                {
+                   count++;
+                }
+                
+            }
+            
+        
+        if(count ==2)
+        {
+            System.out.println(" prime");
+        }
+        else
+        {
+            System.out.println(" not prime");
+        }
+
+
+    }
+}
