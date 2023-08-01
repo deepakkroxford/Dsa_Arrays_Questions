ce756de767e4a67bcb8b7175a2a85432077333ae
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-08-02 00:57:09 +0530

    Update Check_if_two_arrays_are_equal_or_not.java

--- Check_if_two_arrays_are_equal_or_not.java
+++ Check_if_two_arrays_are_equal_or_not.java
@@ -1,9 +1,14 @@
+
+// Given two arrays A and B of equal size N, the task is to find if given arrays are equal or not. Two arrays are said to be equal 
+// if both of them contain same set of elements, arrangements (or permutation) of elements may be different though.
+// Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.
+
 import java.util.Arrays;
 public class Check_if_two_arrays_are_equal_or_not {
     public static void main(String[] args) {
         int a[]={1,2,5,4,0};
         int b[]={2,4,5,0,1};
-
+        // time complexity 0(1)
         Arrays.sort(a);
         Arrays.sort(b);
         int count=0;
@@ -28,3 +33,13 @@ public class Check_if_two_arrays_are_equal_or_not {
         
     }
 }
+
+// Input:
+// N = 5
+// A[] = {1,2,5,4,0}
+// B[] = {2,4,5,0,1}
+// Output: 1
+// Explanation: Both the array can be 
+// rearranged to {0,1,2,4,5}
+// Expected Time Complexity : O(N)
+// Expected Auxilliary Space : O(N)
