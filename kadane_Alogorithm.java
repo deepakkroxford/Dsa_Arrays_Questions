2e002f33efffdd18264db52940912e4fd5825786
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-08-04 00:25:46 +0530

    Update kadane_Alogorithm.java

--- kadane_Alogorithm.java
+++ kadane_Alogorithm.java
@@ -1,14 +1,36 @@
- public class kadane_Alogorithm
- {
+/*
+ * Given an array Arr[] of N integers. 
+   Find the contiguous sub-array(containing at least one number) 
+   which has the maximum sum and return its sum.
+ */
+
+//   Input:
+//   N = 5
+//   Arr[] = {1,2,3,-2,5}
+//   Output:
+//   9
+//   Explanation:
+//   Max subarray sum is 9
+//   of elements (1, 2, 3, -2, 5) which 
+//   is a contiguous subarray.
+public class kadane_Alogorithm {
+
     public static void main(String[] args) {
-        int arr[]={-2,1,-3,4,-1,2,1,-5,4};
-        for(int i =0;i<arr.length;i++)
-        {
-            for(int j=i;j<arr.length;j++)
-            {
-                int sum=0;
-                sum= sum+arr[j];
+        int arr[] = { 1, 2, 3, -2, 5 };
+        int sum = 0;
+        // int maxi = arr[0];
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+            maxi = Math.max(sum, maxi);
+            // if(maxi<sum)
+            // {
+            // maxi=sum;
+            // }
+            if (sum < 0) {
+                sum = 0;
             }
         }
+        System.out.println(maxi);
     }
- }
+}
