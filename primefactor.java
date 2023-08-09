490fcaf7f2a5c3fc491daa16832d066c173783a9
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-08-10 00:44:21 +0530

    basic

---
+++ primefactor.java
@@ -0,0 +1,21 @@
+public class primefactor {
+    public static void main(String[] args) {
+        double n =1021244;
+        for(int i=2;i<=n;i++)
+        { 
+            /*
+             * this loop check if it is fully divisible by i or not
+             * if yes then divide by that number 
+             */
+            
+            while(n%i==0) 
+            {
+              n = n/i;
+              System.out.println(i);
+            }
+            
+        }
+        
+
+    }
+}
