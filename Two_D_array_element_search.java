dac3bad5dd4979cf18d6d158cd802b80a5b0d4a4
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-08-22 21:11:50 +0530

    important

---
+++ Two_D_array_element_search.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Two_D_array_element_search {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("enter the size of row");
+       int row = sc.nextInt();
+
+       System.out.println("enter the size of col");
+       int col = sc.nextInt();
+
+       int matrix [][] = new int [row][col];
+
+       System.out.println("enter the elements");
+       for(int i =0;i<row;i++)
+       {
+        for(int j=0;j<col;j++)
+        {
+            matrix[i][j]= sc.nextInt();
+        }
+       }
+       System.out.println("enter the element to be searched");
+       int x=sc.nextInt();
+        
+       System.out.println("the element at position is");
+       for(int i =0;i<row;i++)
+       {
+        for(int j=0;j<col;j++)
+        {
+           if(matrix[i][j]==x)
+           {
+            System.out.println(i +" "+j);
+           }
+        }
+       }
+
+
+
+
+    }
+}
