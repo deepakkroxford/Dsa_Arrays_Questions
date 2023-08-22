41c0ed8cdbe1cc68e3fb5b5f36070061a2060c57
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-08-22 20:48:43 +0530

    important

---
+++ TwoD_Array_Inisilization.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class TwoD_Array_Inisilization {
+    public static void main(String[] args) {
+
+    /*
+     * In two d array the indexing start with 0 and first comes row
+     * and then column.
+     * 
+     */
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("enter the size of row");
+        int row = sc.nextInt();
+
+        System.out.println("enter the size of coloum");
+        int col = sc.nextInt();
+
+        //initilzing the array
+        int num[][] = new int [row][col];
+        
+        //this loop is taking input from user
+        for(int i =0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                num[i][j]=sc.nextInt();
+            }
+        }
+
+        //and this loop is used for printing the enter element
+        System.out.println("the enterd elemet are");
+        for(int i =0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                System.out.print(num[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
