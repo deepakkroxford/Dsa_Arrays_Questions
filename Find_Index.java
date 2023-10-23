5731daf25efbd443274b00c9cbf514da8318c36e
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-10-23 11:42:28 +0530

    r

--- Find_Index.java
+++ Find_Index.java
@@ -2,11 +2,17 @@ public class Find_Index {
     public static void main(String[] args) {
         int arr[]={1, 2, 3, 4, 5, 5};
         int key =5;
+        
         for(int i =0;i<arr.length;i++)
         {
+            int count = 0;
             if(arr[i]==key)
             {
+
                 System.out.println(i);
+                count++;
+                if(count==1)
+                break;
             }
         }
     }
