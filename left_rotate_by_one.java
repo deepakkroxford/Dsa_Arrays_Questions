e1c594c5ce584cc7112e57b1dfb435b3986f0c49
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2024-01-10 21:35:13 +0530

    imp

---
+++ left_rotate_by_one.java
@@ -0,0 +1,17 @@
+public class left_rotate_by_one {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int temp = arr[0];
+        for(int i=1;i<arr.length;i++)
+        {
+            arr[i-1]=arr[i];
+        }
+        arr[arr.length-1]=temp;
+
+        for(int i =0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
