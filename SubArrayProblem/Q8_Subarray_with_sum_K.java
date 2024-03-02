718de784c3ca1e92ed54cf48e38b1f3f6fd9196d
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2024-03-02 21:42:40 +0530

    very important

--- SubArrayProblem/Q8_Subarray_with_sum_K.java
+++ SubArrayProblem/Q8_Subarray_with_sum_K.java
@@ -1,9 +1,34 @@
 package SubArrayProblem;
 
-class main {
-    int subarraywithsum_K_ON2(int arr[], int k) {
+import java.util.HashMap;
+
+class subarraysumK {
+    int subarraywithsum_K_ON(int arr[],int k ) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int count = 0;
+        int sum = 0;
+        map.put(sum, 1);
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum+arr[i];
+
+            
+            
+
+            if(map.containsKey(sum-k))
+            {
+                count =count +map.get(sum-k);
+            }
+           
+            map.put(sum, map.getOrDefault(sum,0)+1);
+            
+        }
+
+        return count;
+    }
+    int subarraywithsum_K_ON2(int arr[],int k ) {
 
         int count = 0;
+       
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
 
@@ -30,7 +55,6 @@ public class Q8_Subarray_with_sum_K {
          * 
          * 
          * Example 1:
-         * 
          * Input:
          * N = 5
          * Arr = {10 , 2, -2, -20, 10}
@@ -40,8 +64,8 @@ public class Q8_Subarray_with_sum_K {
          * Subarrays: arr[0...3], arr[1...4], arr[3..4]
          * have sum exactly equal to -10.
          * 
-         * Example 2:
          * 
+         * Example 2:
          * Input:
          * N = 6
          * Arr = {9, 4, 20, 3, 10, 5}
@@ -60,7 +84,14 @@ public class Q8_Subarray_with_sum_K {
          * Expected Time Complexity: O(NlogN)
          * Expected Auxiliary Space: O(N)
          * 
-         * 
+         * {3,9,-2,4,1,-7,2,6,-5,8,-3,-7,6,2,1};
+         * 0 ,0, 5, 5, 0, 0
          */
+
+         subarraysumK ans = new subarraysumK();
+         int arr[]={0 ,0, 5, 5, 0, 0};
+         int k =0;
+         System.out.println("subarray with sum k is "+ans.subarraywithsum_K_ON2(arr,k));
+         System.out.println("subarray with sum k is "+ans.subarraywithsum_K_ON(arr,k));
     }
 }
