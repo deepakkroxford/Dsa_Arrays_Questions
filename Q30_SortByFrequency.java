b260b437ee6d22d3c4eb26d80d1a34fded09d913
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2024-08-29 03:21:49 +0530

    important

---
+++ Q30_SortByFrequency.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
+class SortByFrequency {
+    public ArrayList<Integer> sortByFreq(int arr[]) {
+        // step 1 we have to find the frequency of the each element in the array
+        // create a hashmap and then store the frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (!map.containsKey(arr[i])) {
+                map.put(arr[i], 1);
+            } else {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }
+        }
+
+        // step 2 to create and arraylist and add all the number from array
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i=0;i<arr.length;i++) {
+            ans.add(arr[i]);
+        }
+
+        // step 3 coustom sort
+
+        Collections.sort(ans, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                int freqA = map.get(a);
+                int freqB = map.get(b);
+
+                if (freqA != freqB) {
+                    return freqB - freqA;
+                }
+                return a - b;
+            }
+
+        });
+        return ans;
+
+    }
+}
+
+public class Q30_SortByFrequency {
+    public static void main(String[] args) {
+        SortByFrequency sorter = new SortByFrequency();
+        int arr[] = {5, 5, 4, 6, 4};
+        ArrayList<Integer> sortedList = sorter.sortByFreq(arr);
+        System.out.println(sortedList);
+        
+
+    }
+}
--- SubArrayProblem/Q1_Sub_arrayprinting.java
+++ SubArrayProblem/Q1_Sub_arrayprinting.java
@@ -2,7 +2,7 @@ package SubArrayProblem;
 public class Q1_Sub_arrayprinting {
     public static void main(String[] args) {
         
-      int arr[]={1,2,3,4};
+      int arr[]={1,2,3,4,5,6};
        
       for(int i =0;i<arr.length;i++)
       {
@@ -11,7 +11,7 @@ public class Q1_Sub_arrayprinting {
           
             for(int k =i;k<=j;k++)
             {
-                System.out.print(arr[k]);
+                System.out.print(" "+arr[k]);
             }   
             System.out.println();    
             
