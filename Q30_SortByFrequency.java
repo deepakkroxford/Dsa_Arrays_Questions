66a2b44957be6f9e79fc66e90c5c83d51fb7d829
deepakkroxford <deepakkr.oxford@gmail.com>
2025-04-30 09:20:15 +0530

    adidng comment

--- Q30_SortByFrequency.java
+++ Q30_SortByFrequency.java
@@ -44,6 +44,15 @@ class SortByFrequency {
 
 public class Q30_SortByFrequency {
     public static void main(String[] args) {
+        /*
+         * LeetCode:- 1636.
+         * https://leetcode.com/problems/sort-array-by-increasing-frequency/description/
+         * * Given an array of integers nums, sort the array in increasing order based on
+         * the frequency of the values. If multiple values have the same frequency, sort
+         * them in decreasing order.
+         * * Return the sorted array.
+         * 
+         */
         SortByFrequency sorter = new SortByFrequency();
         int arr[] = {5, 5, 4, 6, 4};
         ArrayList<Integer> sortedList = sorter.sortByFreq(arr);
